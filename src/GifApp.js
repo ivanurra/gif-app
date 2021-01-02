203d28c547526a3dd9076249aadaae8e3a0ca5bf
Ivan Urra <ivan.urra.wd@gmail.com>
2021-01-02 10:50:47 +0100

    first commit

--- src/GifApp.js
+++ src/GifApp.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const GifApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        setCategories([...categories, 'HunterXHunter']);
+    }
+
     return (
         <div>
             <h2>GifApp</h2>
             <hr />
+
+            <button onClick={handleAdd}>Add</button>
+
+            <ol>
+                {
+                    categories.map(category =>{
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </div>
     )
 }
