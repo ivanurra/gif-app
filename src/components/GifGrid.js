19d3eee0ac78290e4db1a420c1f52a51cb33e5e6
Ivan Urra <ivan.urra.wd@gmail.com>
2021-01-05 17:01:49 +0100

    first commit

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 export const GifGrid = ({ category }) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        getGifs();
+    },[])
 
     const getGifs = async() => {
 
         // API SEARCH ENDPOINT
-        // You need a key: https://developers.giphy.com/
+        // You need a key: developers.giphy.com
         // URL = api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=YOUR_KEY
         const url = process.env.URL;
         const resp = await fetch( url );
-        const data = await resp.json();
+        const { data } = await resp.json();
 
         const gifs = data.map( img => {
             return {
@@ -22,11 +28,11 @@ export const GifGrid = ({ category }) => {
         console.log(gifs);
     }
 
-    getGifs();
-
     return (
         <div>
             <h3> { category } </h3>
+            <h3> { count } </h3>
+            <button onClick={ ()=> setCount( count + 1 ) }></button>
         </div>
     )
 }
